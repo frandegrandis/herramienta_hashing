09ea8e38a98f0970d3dfd8a3de2026737801ca9a
frandegrandis <frandegrandis@gmail.com>
2022-12-28 12:01:48 -0300

    Reidentacion de archivos

--- UI/zarasa.py
+++ UI/zarasa.py
@@ -2,6 +2,7 @@ from typing import Callable, Union
 
 from customtkinter import CTkFrame, CTkButton, CTkEntry
 
+
 class FloatSpinbox(CTkFrame):
     def __init__(self, *args,
                  width: int = 100,
@@ -19,15 +20,15 @@ class FloatSpinbox(CTkFrame):
         self.grid_columnconfigure((0, 2), weight=0)  # buttons don't expand
         self.grid_columnconfigure(1, weight=1)  # entry expands
 
-        self.subtract_button = CTkButton(self, text="-", width=height-6, height=height-6,
-                                                       command=self.subtract_button_callback)
+        self.subtract_button = CTkButton(self, text="-", width=height - 6, height=height - 6,
+                                         command=self.subtract_button_callback)
         self.subtract_button.grid(row=0, column=0, padx=(3, 0), pady=3)
 
-        self.entry = CTkEntry(self, width=width-(2*height), height=height-6, border_width=0)
+        self.entry = CTkEntry(self, width=width - (2 * height), height=height - 6, border_width=0)
         self.entry.grid(row=0, column=1, columnspan=1, padx=3, pady=3, sticky="ew")
 
-        self.add_button = CTkButton(self, text="+", width=height-6, height=height-6,
-                                                  command=self.add_button_callback)
+        self.add_button = CTkButton(self, text="+", width=height - 6, height=height - 6,
+                                    command=self.add_button_callback)
         self.add_button.grid(row=0, column=2, padx=(0, 3), pady=3)
 
         # default value
