fb3878b6855391cd250b069fa9fa44a7c67446d6
frandegrandis <frandegrandis@gmail.com>
2022-12-29 10:44:58 -0300

    Refactor y arreglo mini bug

--- helpers/utilidades.py
+++ helpers/utilidades.py
@@ -1,14 +1,11 @@
-from sys import getsizeof
-
-
 def bytes_de_string(elemento):
     return bytes(elemento, 'utf-8')
 
 
 def aumentar_bits(input, i):
-    a = int.from_bytes(bytes(input, 'utf-8'), 'big') + i
-    value = a.to_bytes(getsizeof(a) // 8, 'big').decode()
-    return value
+    input_en_numero = int.from_bytes(bytes_de_string(input), 'big') + i
+    return input_en_numero.to_bytes(len(input), 'big').decode()
+
 
 def reducir_bits(input, i):
     return aumentar_bits(input, -i)
