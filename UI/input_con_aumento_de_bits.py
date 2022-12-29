0f94750bd68c35048d534b8ef5d5731c79b7a944
frandegrandis <frandegrandis@gmail.com>
2022-12-29 16:57:22 -0300

    Agregando seleccion de archivos (#8)
    * Se agrega la seleccion de archivos en UI
    
    * Agrego un enum de events
    
    * Agrego hashing de archivos a la UI

--- UI/input_con_aumento_de_bits.py
+++ UI/input_con_aumento_de_bits.py
@@ -1,7 +1,9 @@
+from tkinter.filedialog import askopenfilename
 from typing import Callable, Union
 
 from customtkinter import CTkFrame, CTkButton, CTkEntry
 
+from helpers.events import Events
 from helpers.utilidades import aumentar_bits, reducir_bits
 
 
@@ -14,12 +16,13 @@ class InputConAumentoDeBits(CTkFrame):
                  **kwargs):
         super().__init__(*args, width=width, height=height, **kwargs)
 
+        self.archivo_seleccionado = False
         self.step_size = step_size
         self.command = command
 
         self.configure(fg_color=("gray78", "gray28"))  # set frame color
 
-        self.grid_columnconfigure((0, 2), weight=0)  # buttons don't expand
+        self.grid_columnconfigure((0, 2, 3), weight=0)  # buttons don't expand
         self.grid_columnconfigure(1, weight=1)  # entry expands
 
         self.subtract_button = CTkButton(self, text="-1 bit", width=height - 6, height=height - 6,
@@ -27,11 +30,16 @@ class InputConAumentoDeBits(CTkFrame):
         self.subtract_button.grid(row=0, column=0, padx=(3, 0), pady=3)
 
         self.entry = CTkEntry(self, width=width - (2 * height), height=height - 6, border_width=0)
+        self.entry.bind(Events.on_change.value, self._archivo_deseleccionado)
         self.entry.grid(row=0, column=1, columnspan=1, padx=3, pady=3, sticky="ew")
 
         self.add_button = CTkButton(self, text="+1 bit", width=height - 6, height=height - 6,
                                     command=self.add_button_callback)
         self.add_button.grid(row=0, column=2, padx=(0, 3), pady=3)
+
+        self.boton_archivo = CTkButton(self, text="Archivo", width=height - 6, height=height - 6,
+                                       command=self.cargar_archivo)
+        self.boton_archivo.grid(row=0, column=3, padx=(0, 3), pady=3)
 
         # default value
         self.entry.insert(0, "")
@@ -57,3 +65,20 @@ class InputConAumentoDeBits(CTkFrame):
             return self.entry.get()
         except ValueError:
             return None
+
+    def cargar_archivo(self):
+        path_archivo = askopenfilename()
+        self._archivo_seleccionado()
+        self.mostrar(path_archivo)
+
+    def _archivo_seleccionado(self):
+        self.archivo_seleccionado = True
+        self.add_button.configure(state="disabled")
+        self.subtract_button.configure(state="disabled")
+
+    def _archivo_deseleccionado(self, _event=None):
+        if not self.archivo_seleccionado:
+            return
+        self.archivo_seleccionado = False
+        self.add_button.configure(state="normal")
+        self.subtract_button.configure(state="normal")
---
+++ helpers/events.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class Events(Enum):
+    on_change = "<Key>"
+    on_click = "<Button-1>"
