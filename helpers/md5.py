dcee357571fed37cc660338c14f73d8711b1fa85
frandegrandis <frandegrandis@gmail.com>
2023-01-02 22:30:20 -0300

    Implementacion md5 (#9)
    * Agregamos implementacion MD5
    
    * Pequeño refactor no testeado
    
    * Refactor a las operaciones de MD5
    
    * Agrego seguimiento a nivel de pasos

---
+++ helpers/md5.py
@@ -0,0 +1,108 @@
+from io import BytesIO
+from typing import BinaryIO
+
+import numpy as np
+
+from helpers.iteracion_md5 import IteracionMD5
+from helpers.md5_operations import MD5SelectorDeOperaciones
+from helpers.operaciones_bit_a_bit import rotar_izquierda
+from helpers.utilidades import suma_modular
+
+md5_block_size = 64
+md5_digest_size = 16
+
+"""
+Defino las permutaciones para las 4 rondas
+"""
+
+
+
+class MD5:
+    def __init__(self):
+        self.length: int = 0
+        self.state: tuple[int, int, int, int] = (0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476)
+        self.n_filled_bytes: int = 0
+        self.buf: bytearray = bytearray(md5_block_size)
+        self.iteraciones = []
+
+    def digest(self) -> bytes:
+        return b''.join(x.to_bytes(length=4, byteorder='little') for x in self.state)
+
+    def process(self, stream: BinaryIO) -> None:
+        assert self.n_filled_bytes < len(self.buf)
+
+        view = memoryview(self.buf)  # Utilizado para trabajar a nivel de bytes
+        while bytes_read := stream.read(md5_block_size - self.n_filled_bytes):
+            view[self.n_filled_bytes:self.n_filled_bytes + len(bytes_read)] = bytes_read
+            if self.n_filled_bytes == 0 and len(bytes_read) == md5_block_size:
+                self.iterar()
+                self.length += md5_block_size
+            else:
+                self.n_filled_bytes += len(bytes_read)
+                if self.n_filled_bytes == md5_block_size:
+                    self.iterar()
+                    self.length += md5_block_size
+                    self.n_filled_bytes = 0
+
+    def finalize(self) -> None:
+        assert self.n_filled_bytes < md5_block_size
+
+        self.length += self.n_filled_bytes
+        self.buf[self.n_filled_bytes] = 0b10000000
+        self.n_filled_bytes += 1
+
+        n_bytes_needed_for_len = 8
+
+        if self.n_filled_bytes + n_bytes_needed_for_len > md5_block_size:
+            self.buf[self.n_filled_bytes:] = bytes(md5_block_size - self.n_filled_bytes)
+            self.iterar()
+            self.n_filled_bytes = 0
+
+        self.buf[self.n_filled_bytes:] = bytes(md5_block_size - self.n_filled_bytes)
+        bit_len_64 = (self.length * 8) % (2 ** 64)
+        self.buf[-n_bytes_needed_for_len:] = bit_len_64.to_bytes(length=n_bytes_needed_for_len,
+                                                                 byteorder='little')
+        self.iterar()
+
+    def iterar(self) -> None:
+        round_1_perm = [i for i in range(16)]  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        round_2_perm = [(5 * i + 1) % 16 for i in range(16)]  # [1, 6, 11, 0, 5, 10, 15, 4, 9, 14, 3, 8, 13, 2, 7, 12]
+        round_3_perm = [(3 * i + 5) % 16 for i in range(16)]  # [5, 8, 11, 14, 1, 4, 7, 10, 13, 0, 3, 6, 9, 12, 15, 2]
+        round_4_perm = [(7 * i) % 16 for i in range(16)]  # [0, 7, 14, 5, 12, 3, 10, 1, 8, 15, 6, 13, 4, 11, 2, 9]
+
+        permutaciones = round_1_perm + round_2_perm + round_3_perm + round_4_perm
+        shift = [7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+                 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20,
+                 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+                 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21]
+        sines = np.abs(np.sin(np.arange(64) + 1))
+        sine_randomness = [int(x) for x in np.floor(2 ** 32 * sines)]
+        msg_chunk = self.buf
+        assert len(msg_chunk) == md5_block_size  # 64 bytes, 512 bits
+        palabras_del_bloque = [int.from_bytes(msg_chunk[i:i + 4], byteorder='little') for i in
+                               range(0, md5_block_size, 4)]
+        assert len(palabras_del_bloque) == 16
+
+        a, b, c, d = self.state
+
+        for i in range(md5_block_size):
+            iteracion = IteracionMD5(a=a, b=b, c=c, d=d, bits_a_rotar=(shift[i]), s=(sine_randomness[i]),
+                                     operacion=(MD5SelectorDeOperaciones.operacion_para(i)),
+                                     palabra_a_sumar=(palabras_del_bloque[permutaciones[i]]))
+            a, b, c, d = iteracion.ejecutar()
+            self.iteraciones.append(iteracion)
+
+        self.state = (
+            suma_modular(self.state[0], a),
+            suma_modular(self.state[1], b),
+            suma_modular(self.state[2], c),
+            suma_modular(self.state[3], d),
+        )
+
+    def update(self, s: bytes) -> bytes:
+        self.process(BytesIO(s))
+        self.finalize()
+        return self.digest()
+
+    def hexdigest(self):
+        return self.digest().hex()
---
+++ tests/helpers/test_md5.py
@@ -0,0 +1,42 @@
+import unittest
+
+from helpers.md5 import MD5
+from helpers.md5_operations import F
+from helpers.utilidades import bytes_de_string
+
+
+class TestMD5(unittest.TestCase):
+    def test_md5(self):
+        expectations = {
+            "": "d41d8cd98f00b204e9800998ecf8427e",
+            "a": "0cc175b9c0f1b6a831c399e269772661",
+            "abc": "900150983cd24fb0d6963f7d28e17f72",
+            "message digest": "f96b697d7cb7938d525a2f31aaf161d0",
+            "abcdefghijklmnopqrstuvwxyz": "c3fcd3d76192e4007dfb496cca67e13b",
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789": "d174ab98d277d9f5a5611c2c9f419d9f",
+            "12345678901234567890123456789012345678901234567890123456789012345678901234567890": "57edf4a22be3c955ac49da2e2107b67a",
+        }
+
+        for string, md5_hash in expectations.items():
+            with self.subTest(string=string, md5_hash=md5_hash):
+                hasher = MD5()
+                hasher.update(bytes_de_string(string))
+                self.assertEqual(hasher.hexdigest(), md5_hash)
+
+    def test_se_guardan_todos_los_pasos_luego_de_hashear(self):
+        hasher = MD5()
+        hasher.update(bytes_de_string("Hola"))
+
+        self.assertEqual(64, len(hasher.iteraciones))
+
+    def test_las_iteraciones_tienen_toda_la_info_necesaria(self):
+        hasher = MD5()
+        hasher.update(bytes_de_string("Hola"))
+        iteracion = hasher.iteraciones[1]
+
+        self.assertEqual([0x10325476, 0x5b578b25, 0xefcdab89, 0x98badcfe], iteracion.valores_iniciales())
+        self.assertTrue(isinstance(iteracion.operacion, F))
+        self.assertEqual(128, iteracion.palabra_a_sumar)
+        self.assertEqual(0xe8c7b756, iteracion.constante_s)
+        self.assertEqual(12, iteracion.bits_a_rotar)
+        self.assertEqual([0x98badcfe, 0xda1a0773, 0x5b578b25, 0xefcdab89], iteracion.valores_finales())
