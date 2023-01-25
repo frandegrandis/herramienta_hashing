049c0aea692b048f3f6b08f648c737956cae4f29
frandegrandis <frandegrandis@gmail.com>
2023-01-24 23:21:47 -0300

    Agregamos el seguimiento de bloques (#11)

--- UI/components/caja_de_iteraciones.py
+++ UI/components/caja_de_iteraciones.py
@@ -1,24 +1,26 @@
 from customtkinter import CTkFrame
 
 from UI.components.caja_de_texto import CajaDeTexto
-from helpers.serializador_md5 import serializar_iteracion_md5
 from UI.components.selector_pasos_bloques import SelectorPasosBloques
+from helpers.serializador_md5 import serializar_paso_md5, serializar_bloque_md5
 
 
 class CajaDeIteraciones(CTkFrame):
-    def __init__(self, debugger, *args, **kwargs):
+    def __init__(self, debugger, mostrar_pasos=True, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.debugger = debugger
+        if mostrar_pasos:
+            self.serializar = serializar_paso_md5
+        else:
+            self.serializar = serializar_bloque_md5
         self.selector_pasos_bloques = SelectorPasosBloques(master=self, opciones_pasos=self.pasos(),
                                                            opciones_bloques=self.bloques(),
+                                                           mostrar_pasos=mostrar_pasos,
                                                            on_change=self.recargar)
         self.selector_pasos_bloques.pack()
 
         self.caja_de_texto = CajaDeTexto(self)
         self.caja_de_texto.pack(fill='both', expand=1)
-
-    def pasos(self):
-        return [f"Paso {i}" for i in range(1, 65)]
 
     def mostrar(self, hasher_debugger):
         self.debugger = hasher_debugger
@@ -26,8 +28,12 @@ class CajaDeIteraciones(CTkFrame):
         self.recargar()
 
     def recargar(self):
-        resultado = serializar_iteracion_md5(self.debugger, paso=self.selector_pasos_bloques.paso(), bloque=self.selector_pasos_bloques.bloque())
+        resultado = self.serializar(self.debugger, paso=self.selector_pasos_bloques.paso(),
+                                        bloque=self.selector_pasos_bloques.bloque())
         self.caja_de_texto.mostrar(resultado)
+
+    def pasos(self):
+        return [f"Paso {i}" for i in range(1, 65)]
 
     def bloques(self):
         return [f"Bloque {i}" for i in range(1, self.debugger.cantidad_bloques() + 1)]
--- UI/components/selector_pasos_bloques.py
+++ UI/components/selector_pasos_bloques.py
@@ -2,16 +2,20 @@ from customtkinter import CTkFrame, CTkOptionMenu
 
 
 class SelectorPasosBloques(CTkFrame):
-    def __init__(self, opciones_pasos, opciones_bloques, on_change, *args, **kwargs):
+    def __init__(self, opciones_pasos, opciones_bloques, on_change, mostrar_pasos=True, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._paso = opciones_pasos[0]
         self._bloque = opciones_bloques[0]
         self.on_change = on_change
+        if mostrar_pasos:
+            self.set_up_pasos(opciones_pasos)
+        self.set_up_bloques(opciones_bloques)
+
+    def set_up_pasos(self, opciones_pasos):
         self.selector_pasos = CTkOptionMenu(master=self,
                                             values=opciones_pasos,
                                             command=self.cambio_paso)
         self.selector_pasos.grid(row=0, column=0)
-        self.set_up_bloques(opciones_bloques)
 
     def set_up_bloques(self, opciones_bloques):
         self.selector_bloque = CTkOptionMenu(master=self,
