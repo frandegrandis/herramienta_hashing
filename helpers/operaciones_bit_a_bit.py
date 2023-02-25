9a426094311d8cfd430cc46db5abe56fd2879cc1
Frandegrandis <francisco.degrandis@10pines.com>
2023-02-25 16:03:14 -0300

    pequeño refactor

--- helpers/operaciones_bit_a_bit.py
+++ helpers/operaciones_bit_a_bit.py
@@ -1,18 +1,19 @@
 from bitarray import bitarray
 
 
-def rotar_izquierda(x: int, y: int) -> int:
+def rotar_izquierda(x: int, cantidad_bits_a_rotar: int) -> int:
     """
     Rota los bits de x en y posiciones, ver test para ejemplos
     pensado unicamente para 32 bits
     """
     bits_de_x = bitarray_de_numero(x)
-    primeros_y_bits = bits_de_x[0:y]
+    primeros_y_bits = bits_de_x[0:cantidad_bits_a_rotar]
     # agrego los primeros bits al final
     aux = bits_de_x + primeros_y_bits
     # remuevo los primeros bits.
     aux = aux[len(aux) - len(bits_de_x)::]
     # lo convierto en numero
+    #TODO: cambiar implementacion a : ((x << cantidad_bits_a_rotar) | (x >> (32 - cantidad_bits_a_rotar))) % 2**32
     return bitarray_a_numero(aux)
 
 
