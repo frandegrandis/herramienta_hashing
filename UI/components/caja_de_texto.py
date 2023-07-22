fd4d22fd9c1abd5c273542eb57de0b5c1d6e0388
frandegrandis <frandegrandis@gmail.com>
2023-07-22 16:06:51 -0300

    Ultimos refactors (#24)
    * Cambios para mostrar historial
    
    * Update de requirements
    
    * Agrego creditos
    
    * Remuevo codigo no utilizado/necesario

--- UI/components/caja_de_texto.py
+++ UI/components/caja_de_texto.py
@@ -3,7 +3,7 @@ from customtkinter import CTkTextbox
 
 class CajaDeTexto(CTkTextbox):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, state='disabled', font=('Courier New', 14), **kwargs)
+        super().__init__(*args, state='disabled', font=('Courier New', 14), wrap="word", **kwargs)
 
     def insert(self, index, text, tags=None):
         self.configure(state='normal')
@@ -11,10 +11,18 @@ class CajaDeTexto(CTkTextbox):
         self.configure(state='disabled')
 
     def mostrar(self, texto_a_mostrar):
-        self.configure(state='normal')
         self.vaciar()
-        super().insert("0.0", texto_a_mostrar)
-        self.configure(state='disabled')
+        self.insert("0.0", texto_a_mostrar)
 
     def vaciar(self):
+        self.configure(state='normal')
         super().delete("0.0", "end")
+        self.configure(state='disabled')
+
+    def append(self, texto_a_mostrar):
+        texto_actual = self.get("0.0","end")
+        hay_que_limpiar = not any(funcion_de_hash in texto_actual for funcion_de_hash in ["MD5", "SHA1", "SHA256", "SHA512"])
+        if self.get("0.0") == '\n' or hay_que_limpiar:
+            self.mostrar(texto_a_mostrar)
+            return
+        self.insert("end","\n" + texto_a_mostrar)
--- UI/pantalla_principal.py
+++ UI/pantalla_principal.py
@@ -1,16 +1,19 @@
-from customtkinter import CTk, CTkFrame, CTkButton
-from math import ceil
+import pathlib
+
+from PIL import Image
+from customtkinter import CTk, CTkFrame, CTkButton, CTkImage
 
 from UI.components.input_con_aumento_de_bits import InputConAumentoDeBits
 from UI.mostrador_hash import MostradorHash
 from controllers.hasher_controller import HasherController
 
+PATH = pathlib.Path(__file__).parent.parent.resolve()
 
 class PantallaPrincipal(CTk):
     def __init__(self):
         super().__init__()
 
-        self.title("Hashing")
+        self.title("Remake CriptoRes")
         self.minsize(1280, 720)
 
         self.configurar_grilla()
@@ -26,6 +29,10 @@ class PantallaPrincipal(CTk):
     def botonera(self):
         bottom_frame = CTkFrame(master=self)
         bottom_frame.grid(column=0, row=1)
+        logo_uca = CTkImage(light_image=Image.open(f"{PATH}/UI/resources/Isologotipo_UCA_azul.png"),
+                            dark_image=Image.open(f"{PATH}/UI/resources/Isologotipo_UCA_azul.png"),
+                            size=(30,30)
+        )
         CTkButton(master=bottom_frame, text="Hash MD5", command=self.calcular_hash_md5).grid(column=0, row=0, padx=20, pady=10)
         CTkButton(master=bottom_frame, text="Hash SHA1", command=self.calcular_hash_sha1).grid(column=1, row=0, padx=20, pady=10)
         CTkButton(master=bottom_frame, text="Hash SHA256", command=self.calcular_hash_sha256).grid(column=2, row=0, padx=20, pady=10)
@@ -34,18 +41,19 @@ class PantallaPrincipal(CTk):
         CTkButton(master=bottom_frame, text="Debug SHA1", command=self.debuguear_hash_sha1).grid(column=1, row=1, padx=20, pady=10)
         CTkButton(master=bottom_frame, text="Debug SHA256", command=self.debuguear_hash_sha256).grid(column=2, row=1, padx=20, pady=10)
         CTkButton(master=bottom_frame, text="Debug SHA512", command=self.debuguear_hash_sha512).grid(column=3, row=1, padx=20, pady=10)
+        CTkButton(master=bottom_frame, text="Creditos", command=self.mostrar_creditos, image=logo_uca).grid(column=4, row=0, padx=20, pady=10, rowspan=2)
 
     def calcular_hash_md5(self):
-        self._obtener_hash(HasherController().calcular_hash_md5)
+        self._obtener_hash(HasherController().calcular_hash_md5, "MD5")
 
     def calcular_hash_sha1(self):
-        self._obtener_hash(HasherController().calcular_hash_sha1)
+        self._obtener_hash(HasherController().calcular_hash_sha1, "SHA1")
 
     def calcular_hash_sha256(self):
-        self._obtener_hash(HasherController().calcular_hash_sha256)
+        self._obtener_hash(HasherController().calcular_hash_sha256, "SHA256")
 
     def calcular_hash_sha512(self):
-        self._obtener_hash(HasherController().calcular_hash_sha512)
+        self._obtener_hash(HasherController().calcular_hash_sha512, "SHA512")
 
     def debuguear_hash_md5(self):
         self.resultado_de_hash.mostrar_pasos_md5(HasherController().debugguear_md5(self.valor_a_hashear()))
@@ -67,9 +75,9 @@ class PantallaPrincipal(CTk):
     def alto_pantalla(self):
         return self.winfo_width()
 
-    def _obtener_hash(self, calculo_hash):
+    def _obtener_hash(self, calculo_hash, algoritmo):
         valor_a_hashear = self.valor_a_hashear()
-        self.resultado_de_hash.mostrar_texto(calculo_hash(valor_a_hashear))
+        self.resultado_de_hash.append_texto(f"Aplicando {algoritmo} sobre {valor_a_hashear}: {calculo_hash(valor_a_hashear)}")
 
     def valor_a_hashear(self):
         if self.input_a_hashear.archivo_seleccionado:
@@ -77,3 +85,6 @@ class PantallaPrincipal(CTk):
         else:
             valor_a_hashear = self.input_a_hashear.get()
         return valor_a_hashear
+
+    def mostrar_creditos(self):
+        self.resultado_de_hash.mostrar_texto("Este programa fue realizado como trabajo final de la carrera Ingeniería Informática por el alumno Francisco De Grandis, siendo sus tutores los profesores Germán Bollmann y Marcelo Cipriano.\nBuenos Aires, Julio 2023")
